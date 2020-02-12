b0f249df2427b8f86b12f5e1e2d31132428c7428
Edward Rice <edward.rice@missouri.edu>
2020-02-12 15:05:59 -0800

    Added argument parsing

--- run_seurat.R
+++ run_seurat.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
-library(optparse)
+library(argparser)
 library(Seurat)
 library(dplyr)
 library(ggplot2)
@@ -38,26 +38,39 @@ FindAllConservedMarkers <- function(
     }, levels(seurat@active.ident), data.frame())
 }
 
-options <- commandArgs(trailingOnly = TRUE)
-feature.bc.matrix <- options[1]
-aggregation <- options[2]
-# TODO add these as optparse options
-min.nfeature <- 200
-max.nfeature <- 2000
-max.percent.mt <- 5
-nfeatures <- 2000
-integrate <- TRUE
-nonlinear.normalization <- TRUE
-group.var <- "challenge_status"
-output.dir <- "."
-num.pcs <- 20
+ParseArguments <- function() {
+    p <- arg_parser('Run Seurat')
+    p <- add_argument(p, '--min-nfeature', default=200,
+                      help='minimum number of features to use a cell')
+    p <- add_argument(p, '--max-nfeature', default=5000,
+                      help='maximum number of features to use a cell')
+    p <- add_argument(p, '--max-percent-mt', default=6.0,
+                      help='max % of counts from MT genes to use a cell')
+    p <- add_argument(p, '--nfeatures', default=2000,
+                      help='use top N features [2000]')
+    p <- add_argument(p, '--integrate', flag=TRUE,
+                      help='integrate dataset divided by --group-var')
+    p <- add_argument(p, '--group-var', help='metadata variable to group by')
+    p <- add_argument(p, '--nonlinear', flag=TRUE, help='use SCTransform')
+    p <- add_argument(p, '--output-dir', default='.',
+                      help='output directory for plots and tables')
+    p <- add_argument(p, '--num-pcs', default=20,
+                      help="number of principal components to use")
+    p <- add_argument(p, 'feature-matrix',
+                      help='folder containing feature matrix')
+    p <- add_argument(p, 'aggregation',
+                      help='csv containing metadata, output by Cell Ranger')
+    return(parse_args(p))
+}
+
+argv <- ParseArguments()
 
 # load Cell Ranger output
-seurat.data <- Read10X(data.dir = feature.bc.matrix)
-seurat <- CreateSeuratObject(seurat.data, min.cells = 3, min.features = 200)
+seurat.data <- Read10X(data.dir = argv$feature_matrix)
+seurat <- CreateSeuratObject(seurat.data, min.cells = 3, min.features = 100)
 
 # add metadata to Seurat object
-meta.data <- read.csv(aggregation)
+meta.data <- read.csv(argv$aggregation)
 seurat <- add.meta.data(seurat, meta.data)
 seurat[["percent.mt"]] <- PercentageFeatureSet(seurat, pattern = "^MT-")
 seurat[["percent.ribo"]] <- PercentageFeatureSet(seurat, pattern = "^RP[LS]")
@@ -65,22 +78,23 @@ seurat[["percent.ribo"]] <- PercentageFeatureSet(seurat, pattern = "^RP[LS]")
 # make a diagnostic plot to help with filtering
 p <- ggplot(seurat@meta.data, aes(nCount_RNA, nFeature_RNA, color=percent.mt))
 p <- p + geom_point(size=0.2)
-p <- p + geom_hline(yintercept=min.nfeature, color='red')
-p <- p + geom_hline(yintercept=max.nfeature, color='red')
-ggsave(paste(output.dir, 'feature_plot.pdf', sep='/'), plot=p)
+p <- p + geom_hline(yintercept=argv$min_nfeature, color='red')
+p <- p + geom_hline(yintercept=argv$max_nfeature, color='red')
+ggsave(paste(argv$output_dir, 'feature_plot.pdf', sep='/'), plot=p)
 
 # do some basic filtering
 seurat <- subset(
     seurat,
-    subset = nFeature_RNA > min.nfeature & nFeature_RNA < max.nfeature &
-        percent.mt < max.percent.mt
+    subset = (nFeature_RNA > argv$min_nfeature
+              & nFeature_RNA < argv$max_nfeature
+              & percent.mt < argv$max_percent_mt)
 )
 
 # normalize, find variable features, and scale. Integrate datasets if
 # requested, and use non-linear normalization if requested.
-if (integrate) {
-    seurat.list <- SplitObject(seurat, split.by = group.var)
-    if (nonlinear.normalization) {
+if (argv$integrate) {
+    seurat.list <- SplitObject(seurat, split.by = argv$group_var)
+    if (argv$nonlinear) {
         seurat.list <- lapply(seurat.list, function (x) {
             SCTransform(
                 x, vars.to.regress = c("percent.mt", "percent.ribo"),
@@ -88,7 +102,7 @@ if (integrate) {
                 variable.features.n = nfeatures)
         })
         seurat.features <- SelectIntegrationFeatures(seurat.list,
-                                                     nfeatures=nfeatures)
+                                                     nfeatures=argv$nfeatures)
         options(future.globals.maxSize = 891289600)
         seurat.list <- PrepSCTIntegration(seurat.list,
                                           anchor.features = seurat.features)
@@ -98,44 +112,50 @@ if (integrate) {
         seurat <- IntegrateData(seurat.anchors, normalization.method = "SCT")
     } else {
         seurat.list <- lapply(seurat.list, function (x) {
-            FindVariableFeatures(NormalizatData(x), nfeatures=nfeatures)
+            FindVariableFeatures(NormalizatData(x), nfeatures=argv$nfeatures)
         })
         seurat.anchors <- FindIntegrationAnchors(seurat.list)
         seurat <- IntegrateData(seurat.anchors)
     }
 } else {
-    if (nonlinear.normalization) {
+    if (argv$nonlinear) {
         seurat <- SCTransform(
             seurat,
             vars.to.regress = c("percent.mt", "percent.ribo"),
             do.scale = TRUE)
     } else {
         seurat <- NormalizeData(seurat)
-        seurat <- FindVariableFeatures(seurat, nfeatures = nfeatures)
+        seurat <- FindVariableFeatures(seurat, nfeatures = argv$nfeatures)
         seurat <- ScaleData(seurat)
     }
 }
 
 # do several super-standard analysis steps
-seurat <- FindClusters(FindNeighbors(RunUMAP(RunPCA(seurat), dims=1:num.pcs)))
+seurat <- FindClusters(FindNeighbors(RunUMAP(RunPCA(seurat),
+                                             dims=1:argv$num_pcs)))
 
 # make an elbow plot to help choose number of PCs
-p <- ElbowPlot(seurat) + geom_vline(xintercept = num.pcs)
-ggsave(paste(output.dir, 'elbow.pdf', sep='/'), plot=p)
+p <- ElbowPlot(seurat, ndims = argv$num_pcs + 10)
+p <- p + geom_vline(xintercept = argv$num_pcs)
+ggsave(paste(argv$output_dir, 'elbow.pdf', sep='/'), plot=p)
 
 # make some UMAP plots
 p <- DimPlot(seurat, group.by="library_id")
-ggsave(paste(output.dir, 'umap.batches.pdf', sep='/'), plot=p)
-if (group.var == '') {
+ggsave(paste(argv$output_dir, 'umap.batches.pdf', sep='/'), plot=p)
+if (is.na(argv$group_var)) {
     p <- DimPlot(seurat, label=TRUE) + NoLegend()
 } else {
-    p <- DimPlot(seurat, group.by=group.var) # TODO increase width of this plot
-    ggsave(paste(output.dir, 'umap.groups.pdf', sep='/'), plot=p)
-    p <- DimPlot(seurat, split.by=group.var, label=TRUE) + NoLegend()
+    # TODO increase width of this plot
+    p <- DimPlot(seurat, group.by=argv$group_var)
+    ggsave(paste(argv$output_dir, 'umap.groups.pdf', sep='/'), plot=p)
+    p <- DimPlot(seurat, split.by=argv$group_var, label=TRUE) + NoLegend()
 }
-ggsave(paste(output.dir, 'umap.clusters.pdf', sep='/'), plot=p)
+ggsave(paste(argv$output_dir, 'umap.clusters.pdf', sep='/'), plot=p)
 
 # TODO find conserved markers
 # TODO make a heatmap
 
 # TODO differential expression per cell type between groups
+
+
+
