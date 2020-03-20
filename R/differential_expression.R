11219e03301d9dea4543990d12d06ec3427912a5
Edward S. Rice <edward.rice@missouri.edu>
2020-03-19 18:29:16 -0700

    Added lots of Roxygen documentation

--- R/differential_expression.R
+++ R/differential_expression.R
@@ -1,6 +1,33 @@
+#' Calculate the cellular detection rate
+#'
+#' Given a feature X cell table of raw counts, calculate the cellular detection
+#' rate for each cell. The cellular detection rate of a cell is a number between
+#' 0 and 1 representing the fraction of the total number of features measured
+#' that have a count > 0 in that cell. It is useful as a covariate in
+#' differential expression analyses.
+#'
+#' @param counts A feature X cell table of raw counts.
+#' @return A vector of cellular detection rate per cell
+#' @examples
+#' cdr <- calc.cdr(seurat@assays$RNA@counts)
 calc.cdr <- function(counts) scale(colMeans(as.matrix(counts) > 0))
 
-# this is based on https://github.com/csoneson/conquer_comparison/blob/master/scripts/apply_edgeRQLFDetRate.R
+#' Run edgeR to perform DE analysis
+#'
+#' Run edgeR to assign a p-value to each feature for the null hypothesis that
+#' this feature is not significantly differently expressed between the cells
+#' of a given cluster between the two levels of a condition. This gives edgeR
+#' raw counts, and uses cellular detection rate as a covariate. Based on
+#' \link{https://github.com/csoneson/conquer_comparison/blob/master/scripts/apply_edgeRQLFDetRate.R}
+#'
+#' @param seurat A seurat object.
+#' @param cluster The number of a cluster to perform DE analysis on
+#' @param grouping.var The name of the metadata variable used to group the
+#'   samples into two groups to compare.
+#' @return A \code{data.frame} with row names as feature names, and the columns
+#'   \code{p} and \code{fold.change}.
+#' @examples
+#' de.results <- RunEdgeR(seurat, 0, 'challenge_status')
 RunEdgeR <- function(
     seurat,
     cluster,
@@ -26,7 +53,20 @@ RunEdgeR <- function(
                       row.names = rownames(tt$table)))
 }
 
-# based on https://github.com/csoneson/conquer_comparison/blob/master/scripts/apply_MASTcpmDetRate.R
+#' Run MAST to perform DE analysis.
+#'
+#' Run MAST to assign a p-value to each feature for the null hypothesis that
+#' this feature is not significantly differently expressed in the cells of a
+#' given cluster between two levels of a condition. This function gives MAST
+#' the CPM (counts per million) normalized expression level, and includes
+#' cellular detection rate as a covariate. Based on
+#' \link{https://github.com/csoneson/conquer_comparison/blob/master/scripts/apply_MASTcpmDetRate.R}
+#'
+#' @inheritParams RunEdgeR
+#' @return A \code{data.frame} with row names as feature names, and the columns
+#'   \code{p} and \code{fold.change}.
+#' @examples
+#' de.results <- RunMAST(seurat, 0, 'challenge_status')
 RunMAST <- function(
     seurat,
     cluster,
@@ -65,7 +105,20 @@ RunMAST <- function(
                       row.names = rownames(mast[, 'hurdle',])))
 }
 
-# based on https://github.com/csoneson/conquer_comparison/blob/master/scripts/apply_Wilcoxon.R
+#' Run Wilcoxon tests to perform DE analysis.
+#'
+#' Use Wilcoxon tests to assign a p-value to each feature for the null
+#' hypothesis that this feature is not significantly differently expressed in
+#' the cells of a given cluster between two levels of a condition. This function
+#' uses the CPM (counts per million) normalized expression levels as input to
+#' the Wilcoxon test. Based on
+#' \link{https://github.com/csoneson/conquer_comparison/blob/master/scripts/apply_Wilcoxon.R}
+#'
+#' @inheritParams RunEdgeR
+#' @return A \code{data.frame} with row names as feature names, and the columns
+#'   \code{p} and \code{fold.change}.
+#' @examples
+#' de.results <- RunMAST(seurat, 0, 'challenge_status')
 RunWilcoxon <- function(
     seurat,
     cluster,
@@ -92,6 +145,19 @@ RunWilcoxon <- function(
                       row.names = names(wilcox.p)))
 }
 
+#' Run differential expression analysis on every cluster
+#'
+#' Use a given function to perform differential expression analysis between two
+#' conditions on every cluster in a Seurat object.
+#'
+#' @param method A function that takes a seurat object, a cluster number, and a
+#'   grouping variable, and returns a \code{data.frame} with row names as
+#'   feature names, and the columns \code{p} and \code{fold.change}. See, for
+#'   example, \code{\link{RunEdgeR}}
+#' @inheritParams RunEdgeR
+#' @return A \code{data.frame} containing one row for each cluster/feature
+#'   combination, with columns \code{p}, \code{fold.change}, \code{cluster}, and
+#'   \code{feature}.
 FindAllClusterDE <- function(
     seurat,
     grouping.var,
--- R/seurat_helpers.R
+++ R/seurat_helpers.R
@@ -1,3 +1,21 @@
+#' Add metadata to a Seurat object from a data frame
+#'
+#' When creating a Seurat object with, for example, \code{Read10X}, no metadata
+#' is loaded automatically, even though \code{cellranger aggregate} gives you
+#' a nice aggregation csv. The cell barcodes just contain a numerical suffix to
+#' indicate which library they're from. It's useful to have all the metadata
+#' associated with each library as part of the Seurat object, so this function
+#' parses each cell barcode to figure out what gem group the cell came from,
+#' and then applies gem-group-level metadata contained in a table to the cell.
+#'
+#' @param seurat A seurat object.
+#' @param meta.data A table containing gem-group-level metadata to be added to
+#'   \code{seurat@meta.data}. Rows must be in order of gem group, as output by
+#'   \code{cellranger aggregate}.
+#' @return The seurat object with metadata added.
+#' @examples
+#' meta.data <- read.csv('aggregation.csv')
+#' seurat <- add.meta.data(seurat, meta.data)
 add.meta.data <- function(seurat, meta.data) {
     gem.group <- as.numeric(sapply(strsplit(rownames(
         seurat@meta.data), split="-"), "[[", 2))
@@ -12,8 +30,20 @@ add.meta.data <- function(seurat, meta.data) {
     return(AddMetaData(seurat, df))
 }
 
-# this does what FindAllMarkers does, except it uses FindConservedMarkers
-# inside instead of FindMarkers.
+#' Find conserved markers for each cluster
+#'
+#' Find conserved markers for each cluster. Basically, this does what
+#' \link{\code{Seurat::FindAllMarkers}} does, except it uses
+#' \link{\code{Seurat::FindConservedMarkers}} inside instead of plain old
+#' \link{\code{Seurat::FindMarkers}}.
+#'
+#' @param seurat A seurat object.
+#' @param grouping.var The name of the metadata variable to use to separate the
+#'   cells into two conditions.
+#' @param test.use String specifying the test to pass to \code{FindMarkers}.
+#' @return A \code{data.frame} of the outputs of all calls to
+#'   \code{FindConservedMarkers}, row-bound together with the added columns
+#'   \code{cluster} and \code{feature}.
 FindAllConservedMarkers <- function(
     seurat,
     grouping.var,
