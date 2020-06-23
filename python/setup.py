2be77d92574ed4e426dd5ef2626718c770b6c569
Edward S. Rice <edward.rice@missouri.edu>
2020-06-23 12:19:35 -0700

    Give new script .py extension and add to setup

--- python/setup.py
+++ python/setup.py
@@ -3,5 +3,8 @@ import setuptools
 setuptools.setup(
     name="singlecell",
     packages=setuptools.find_packages(),
-    scripts=['mt_prepend_gtf.py']
+    scripts=[
+        'mt_prepend_gtf.py',
+        'create_cellranger_ref.py',
+    ]
 )
