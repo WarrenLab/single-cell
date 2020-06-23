ca9caae09f9c15893ff3c3cbd0ec4bd91acb53e1
Edward S. Rice <edward.rice@missouri.edu>
2020-06-23 13:08:47 -0700

    Linted script to conform to PEP8

--- python/create_cellranger_ref.py
+++ python/create_cellranger_ref.py
@@ -1,61 +1,137 @@
-#!/bin/env python3
-"""This follows the recommendations of 10X (see https://support.10xgenomics.com/single-cell-gene-expression/software/pipelines/latest/advanced/references), including the optional creation of a "pre-mRNA" version of a reference for nuclei data.
+#!/usr/bin/env python3
+"""Create a single-cell genome reference using cellranger.
 
-It also uses "mt_prepend_gtf.py" to prepend "MT-" to each gene name  from the "MT" chromosome, if that gene name does not already start with those three characters.
+This follows the recommendations of 10X (see
+https://support.10xgenomics.com/single-cell-gene-expression/software/pipelines/latest/advanced/references),
+including the optional creation of a "pre-mRNA" version of a reference
+for nuclei data.
+
+It also uses "mt_prepend_gtf.py" to prepend "MT-" to each gene name
+from the "MT" chromosome, if that gene name does not already start with
+those three characters.
 """
 
-from random import randint
 import argparse
 import os
 import subprocess
 
-f'You need late enough Python 3 to use f-strings' if f'you want this to run or' else f'this line fails!'
 
-def local_replace_ext (file_name,replacement):
+def local_replace_ext(file_name, replacement):
+    """
+    Replace a file name's extension.
+
+    Args:
+        file_name: a path-like object containing the name of the file
+            whose extension will be replaced
+        replacement: string containing replacement extension
+
+    Returns:
+        string containing modified filename
+    """
     local_file_name = os.path.basename(file_name)
     return os.path.splitext(local_file_name)[0] + replacement
 
-DEBUG = False
 
 # module loading (system specific. This works on MU's Lewis cluster)
 MODULE_LOAD_TEXT = """module load biocompute/biocompute-modules
 module load gffread/gffread-0.11.3
 source /cluster/biocompute/software/cellranger/cellranger-3.1.0/sourceme.bash"""
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser( description='create CellRanger reference using a FASTA reference with its corresponding GTF or GFF3')
 
-    parser.add_argument('--gtf',         type=str, help='name of GTF file from ensembl (either --gtf or -gff is required)',                     )
-    parser.add_argument('--gff',         type=str, help='name of GFF3 file to be converted (either --gtf or -gff is required)',                 )
-    parser.add_argument('--fasta',       type=str, help='name of reference FASTA file to be converted',          required=True                  )
-    parser.add_argument('--cpus',        type=int, help='cpu cores to request (default 12)',                     default=24                     )
-    parser.add_argument('--mem',         type=str, help='Total RAM to allocate in GB (default: "300")',          default='300'                  )
-    parser.add_argument('--partition',   type=str, help='partition to use',                                      default='Lewis,hpc5,hpc6,BioCompute')
-    parser.add_argument('--genome',      type=str, help='name of genome'                                                                        )
-    parser.add_argument('--no-run',                help="Do not run script immediately after creating it.", action='store_false', dest='run'    )
-    parser.add_argument('--run',                   help="Run script immediately after creating it.",        action='store_true'                 )
-    parser.add_argument('--pre-mRNA',              help="Convert GTF 'transcripts' to behave as one large exon (thus pre-mRNA reads map to the transcriptome, may be useful for nuclei data)",
-            action='store_true', default=False, dest='pre_mrna' )
-    parser.add_argument('--no-pre-mRNA',           help="Do not modify GTF to convert each transcripts into one 'big exon'", action='store_false', dest='pre_mrna' )
-    parser.add_argument('--account',               help="SLURM account to use", default='warrenlab' )
+def parse_args():
+    """parse command line arguments"""
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    args = parser.parse_args()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        '--gtf',
+        type=str,
+        help='name of GTF file from ensembl',
+    )
+    group.add_argument(
+        '--gff',
+        type=str,
+        help='name of GFF3 file to be converted',
+    )
+
+    parser.add_argument(
+        '--fasta',
+        type=str,
+        help='name of reference FASTA file to be converted',
+        required=True,
+    )
+    parser.add_argument(
+        '--cpus',
+        type=int,
+        help='cpu cores to request',
+        default=24,
+    )
+    parser.add_argument(
+        '--mem',
+        type=str,
+        help='Total RAM to allocate in GB',
+        default='300',
+    )
+    parser.add_argument(
+        '--partition',
+        type=str,
+        help='partition to use',
+        default='Lewis,hpc5,hpc6,BioCompute',
+    )
+    parser.add_argument(
+        '--genome',
+        type=str,
+        help='name of genome',
+    )
+    parser.add_argument(
+        '--no-run',
+        help="Do not run script immediately after creating it.",
+        action='store_false',
+        dest='run',
+    )
+    parser.add_argument(
+        '--run',
+        help="Run script immediately after creating it.",
+        action='store_true',
+    )
+    parser.add_argument(
+        '--pre-mRNA',
+        help='Convert GTF transcripts to behave as one large exon '
+             '(thus pre-mRNA reads map to the transcriptome, may be '
+             'useful for nuclei data)',
+        action='store_true',
+        default=False,
+        dest='pre_mrna',
+    )
+    parser.add_argument(
+        '--account',
+        help="SLURM account to use",
+        default='warrenlab',
+    )
+    parser.add_argument(
+        '--debug',
+        action='store_true',
+        help='run in debug mode',
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    """main method of script"""
+    args = parse_args()
 
     # Specify that the MT genes may be renamed
     genome = args.genome + ".MT-renamed"
 
-    if args.gtf or args.gff:
-        pass
-    else:
-        print('Either --gtf or --gff are required')
-        exit(1)
-
-    job_name = f"create_gtf__{args.genome}";
+    job_name = f"create_gtf__{args.genome}"
     if args.pre_mrna:
         job_name += ".pre-mRNA"
         genome += ".pre-mRNA"
     gtf_first = ''
-    create_gtf = ''
     filter_command = ''
     filter_comment = ''
     gxf = ''
@@ -63,7 +139,7 @@ if __name__ == '__main__':
         gtf_first = args.gtf
         gxf = args.gtf
     elif args.gff:
-        gtf_first = local_replace_ext(args.gff,f".converted_from_gff.gtf")
+        gtf_first = local_replace_ext(args.gff, ".converted_from_gff.gtf")
         filter_comment += '# convert GFF to a GTF file and'
         filter_command += f"gffread {args.gff} -g {args.fasta} -T -o {gtf_first} "
         gxf = args.gff
@@ -71,27 +147,30 @@ if __name__ == '__main__':
     base_gxf = ""
 
     if args.pre_mrna:
-        base_gxf = local_replace_ext(gxf,".pre-mRNA.x")
+        base_gxf = local_replace_ext(gxf, ".pre-mRNA.x")
     else:
-        base_gxf = local_replace_ext(gxf,".x")
+        base_gxf = local_replace_ext(gxf, ".x")
 
-    gtf_temp          = local_replace_ext(base_gxf,f".only_lines_with_gene_id.gtf")
-    gtf_MT_prepended  = local_replace_ext(gtf_temp,f".MT-renamed.gtf")
-    gtf_final         = local_replace_ext(gtf_MT_prepended,f".filtered.gtf")
+    gtf_temp = local_replace_ext(base_gxf, ".only_lines_with_gene_id.gtf")
+    gtf_mt_prepended = local_replace_ext(gtf_temp, ".MT-renamed.gtf")
+    gtf_final = local_replace_ext(gtf_mt_prepended, ".filtered.gtf")
 
-    log_final         = local_replace_ext(base_gxf,".STAR.log")
+    log_final = local_replace_ext(base_gxf, ".STAR.log")
 
     if args.pre_mrna:
-        filter_comment += '# convert transcripts into exons, keeping only those containing "gene_id" '
+        filter_comment += (
+            '# convert transcripts into exons, keeping only '
+            + 'those containing "gene_id"')
         filter_command +=  \
         """awk 'BEGIN{FS="\\t"; OFS="\\t"} $3 == "transcript"{ $3="exon"; print}' """ + \
         f"{gtf_first} | grep -w gene_id > {gtf_temp} "
     else:
         filter_comment += '# keep only those GTF records containing "gene_id" '
-        filter_command +=  f"grep -w gene_id {gtf_first} > {gtf_temp} "
+        filter_command += f"grep -w gene_id {gtf_first} > {gtf_temp} "
 
-    #For every gene name in the "MT" chromosome, prepend "MT-" if it isn't already present 
-    prepend_MT_command = f"mt_prepend_gtf.py {gtf_temp} > {gtf_MT_prepended}"
+    # For every gene name in the "MT" chromosome, prepend "MT-" if it
+    # isn't already present
+    prepend_mt_command = f"mt_prepend_gtf.py {gtf_temp} > {gtf_mt_prepended}"
 
     # partition here are system specific
     filled_template = """#!/bin/bash
@@ -153,34 +232,38 @@ mv Log.out {log_final}
         #Genome info, files
         genome=genome,
         fasta=args.fasta,
-        gtf_MT_prepended=gtf_MT_prepended,
+        gtf_MT_prepended=gtf_mt_prepended,
         gtf_final=gtf_final,
 
         #Previously build bash commands
         filter_command=filter_command,
         filter_comment=filter_comment,
-        prepend_MT_command=prepend_MT_command,
+        prepend_MT_command=prepend_mt_command,
 
         log_final=log_final
         )
 
-    if not DEBUG:
+    if not args.debug:
         filled_template = filled_template + \
             "# clean up"      + "\n" +      \
             "rm " + gtf_temp  + "\n" +      \
-            "rm " + gtf_MT_prepended  + "\n"
+            "rm " + gtf_mt_prepended  + "\n"
 
     job_script_name = job_name + '.sbatch'
-    with open(job_script_name, "w") as fh:
-        fh.write(filled_template)
+    with open(job_script_name, "w") as file_handle:
+        file_handle.write(filled_template)
 
     # Run batch file (if requested)
     if args.run:
         output = subprocess.run(
-                    ['sbatch', job_script_name],
-                    stdout=subprocess.PIPE
-                 )
+            ['sbatch', job_script_name],
+            stdout=subprocess.PIPE
+        )
         print(output.stdout.decode('utf-8'))
 
-# The only "filtering" really done above is keeping only lines containing "gene_id". You can do more specific filters.
-# (See https://support.10xgenomics.com/single-cell-gene-expression/software/pipelines/latest/advanced/references)
+# The only "filtering" really done above is keeping only lines
+# containing "gene_id". You can do more specific filters. See
+# https://support.10xgenomics.com/single-cell-gene-expression/software/pipelines/latest/advanced/references
+
+if __name__ == '__main__':
+    main()
--- python/setup.py
+++ python/setup.py
@@ -3,6 +3,7 @@ import setuptools
 setuptools.setup(
     name="singlecell",
     packages=setuptools.find_packages(),
+    install_requires=['python>=3.6'],
     scripts=[
         'mt_prepend_gtf.py',
         'create_cellranger_ref.py',
