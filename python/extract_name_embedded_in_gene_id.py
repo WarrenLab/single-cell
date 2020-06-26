819fd29e9a00bfd5a43d6c94087f7d75e83762a4
Christopher Bottoms <molecules@users.noreply.github.com>
2020-06-26 01:43:38 -0500

    Extract gene name from gene ID
    If there is no explicit gene name, but there is one embedded in the gene_id (i.e. it starts with "gene-"), then extract it.

---
+++ python/extract_name_embedded_in_gene_id.py
@@ -0,0 +1,37 @@
+#!/bin/env python3
+"""
+Extract gene name from gene ID if gene name not defined and if gene ID starts with "gene-"
+"""
+import argparse
+
+import gtfez
+
+def parse_args():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        'gtf',
+        type=argparse.FileType('r'),
+        help='location of GTF file',
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+def main(args):
+    for line in args.gtf:
+        if line.startswith('#'):
+            print(line.rstrip())
+            continue
+
+        record = gtfez.GTFRecord(line)
+        if 'gene_name' not in record.attributes and 'gene_id' in record.attributes and record.attributes['gene_name'].lower().startswith('gene-'):
+            if ('gene_name' in record.attributes
+                    and not record.attributes['gene_name'].startswith('MT-')):
+            record.attributes['gene_name'] = record.attributes['gene_id'][5:]
+        print(record)
+
+# command line interface (making this a modulino)
+if __name__ == '__main__':
+    main(parse_args())
+
