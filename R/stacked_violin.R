90e1c39a1477c4cff31581e536262d8e2ea948d9
Christopher Bottoms <molecules@cpan.org>
2020-08-24 10:37:25 -0500

    Adjusted spacing to make it more readable and removed end-of-line spaces

--- R/stacked_violin.R
+++ R/stacked_violin.R
@@ -9,25 +9,25 @@ library(ggplot2)
 #' @param features The names of the genes to plot, each one getting its own violin plot row
 ## ... pass any arguments to VlnPlot in Seurat
 #' @return A single violin plot for inclusion in larger plot
-modify_vlnplot<- function(obj, 
-                          feature, 
-                          pt.size = 0, 
+modify_vlnplot <- function(obj,
+                          feature,
+                          pt.size = 0,
                           plot.margin = unit(c(-0.75, 0, -0.75, 0), "cm"),
                           ...) {
-  p<- VlnPlot(obj, features = feature, pt.size = pt.size, ... )  + 
-    xlab("") + ylab(feature) + ggtitle("") + 
-    theme(legend.position = "none", 
-          axis.text.x = element_blank(), 
-          axis.ticks.x = element_blank(), 
-          axis.title.y = element_text(size = rel(1), angle = 0), 
-          axis.text.y = element_text(size = rel(1)), 
-          plot.margin = plot.margin ) 
+  p <- VlnPlot(obj, features = feature, pt.size = pt.size, ... )  +
+    xlab("") + ylab(feature) + ggtitle("") +
+    theme(legend.position = "none",
+          axis.text.x = element_blank(),
+          axis.ticks.x = element_blank(),
+          axis.title.y = element_text(size = rel(1), angle = 0),
+          axis.text.y = element_text(size = rel(1)),
+          plot.margin = plot.margin )
   return(p)
 }
 
 ## extract the max value of the y axis
-extract_max<- function(p){
-  ymax<- max(ggplot_build(p)$layout$panel_scales_y[[1]]$range$range)
+extract_max <- function(p){
+  ymax <- max(ggplot_build(p)$layout$panel_scales_y[[1]]$range$range)
   return(ceiling(ymax))
 }
 
@@ -39,23 +39,23 @@ extract_max<- function(p){
 #' @return A stacked violin plot
 #' @export
 ## main function
-StackedVlnPlot<- function(seurat, features,
-                          pt.size = 0, 
+StackedVlnPlot <- function(seurat, features,
+                          pt.size = 0,
                           plot.margin = unit(c(-0.75, 0, -0.75, 0), "cm"),
                           ...) {
-  
-  plot_list<- purrr::map(features, function(x) modify_vlnplot(obj = seurat,feature = x, ...))
-  
+
+  plot_list <- purrr::map(features, function(x) modify_vlnplot(obj = seurat,feature = x, ...))
+
   # Add back x-axis title to bottom plot. patchwork is going to support this?
-  plot_list[[length(plot_list)]]<- plot_list[[length(plot_list)]] +
+  plot_list[[length(plot_list)]] <- plot_list[[length(plot_list)]] +
     theme(axis.text.x=element_text(), axis.ticks.x = element_line())
-  
-  # change the y-axis tick to only max value 
-  ymaxs<- purrr::map_dbl(plot_list, extract_max)
-  plot_list<- purrr::map2(plot_list, ymaxs, function(x,y) x + 
-                            scale_y_continuous(breaks = c(y)) + 
+
+  # change the y-axis tick to only max value
+  ymaxs <- purrr::map_dbl(plot_list, extract_max)
+  plot_list <- purrr::map2(plot_list, ymaxs, function(x,y) x +
+                            scale_y_continuous(breaks = c(y)) +
                             expand_limits(y = y))
 
-  p<- patchwork::wrap_plots(plotlist = plot_list, ncol = 1)
+  p <- patchwork::wrap_plots(plotlist = plot_list, ncol = 1)
   return(p)
 }
